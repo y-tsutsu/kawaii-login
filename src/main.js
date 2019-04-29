dd3190d2505581c424ff148f2a7b559f085db3f6
tsutsu <tsutsu0928@gmail.com>
2019-04-29 23:54:07 +0900

    firebaseにdeploy

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: 'AIzaSyBUviaECa8U9LvR11QeBoqtdVJiUY1C8Xc',
+  authDomain: 'kawaii-login-c7863.firebaseapp.com',
+  databaseURL: 'https://kawaii-login-c7863.firebaseio.com',
+  projectId: 'kawaii-login-c7863',
+  storageBucket: 'kawaii-login-c7863.appspot.com',
+  messagingSenderId: '556677250879'
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
